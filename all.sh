19179e3c353b842b07fef8d415851fefd6895edf
mrandersen <mrandersen2004@yandex.ru>
2019-04-16 17:19:23 +0300

    corrections

--- all.sh
+++ all.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 cd c && gcc -O3 -march=native -o gen gen.c && ./gen && cd ..
-cd go && go build gen.go && ./gen && cd ..
+
+cd go && go build -gccgoflags='-O3 -march=native' gen.go && ./gen && cd ..
+
 cd c++ && g++ -O3 -march=native -o gen gen.cpp && ./gen && cd ..
+
 cd java && java src/com/gen/Main.java && cd ..
+
 cd php && php gen.php && cd ..
+
 cd js && node gen.js && cd ..
+
 cd python && python3.7 gen.py && cd ..
